a4f6b04a552271ae65b2375cddc860dc28ac1b98
Łukasz Gębicki <kontakt@gebickionline.pl>
2015-10-17 13:31:07 +0200

    - refactoring of the tests - body added to request

--- src/main/java/pl/gebickionline/restclient/RestClient.java
+++ src/main/java/pl/gebickionline/restclient/RestClient.java
@@ -3,20 +3,24 @@ package pl.gebickionline.restclient;
 import org.apache.http.*;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.*;
+import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.*;
 
 public class RestClient {
     public static final String CHARSET_NAME = "UTF-8";
     private final HttpRequestBase request;
     private byte[] body;
+    private Map<String, String> headers;
 
     private RestClient(HttpRequestBase request) {
         this.request = request;
-        this.request.addHeader("accept", "text/plain");
+        this.headers = new HashMap<>();
+        this.headers.put("accept", "text/plain");
     }
 
     public static RestClient put(String targetURL) {
@@ -45,8 +49,8 @@ public class RestClient {
         return this;
     }
 
-    public RestClient header(String name, String value) {
-        this.request.addHeader(name, value);
+    public RestClient header(String name, String... values) {
+        this.headers.put(name, String.join(",", values));
         return this;
     }
 
@@ -66,11 +70,21 @@ public class RestClient {
     }
 
     private HttpResponse executeRequest() {
+        headers.entrySet()
+                .forEach(header -> request.addHeader(header.getKey(), header.getValue()));
+
+        if (body != null)
+            addBodyToRequest();
         try {
             return httpClient().execute(request);
         } catch (IOException e) {
             throw new ExecuteRequestException(e);
         }
+    }
+
+    private void addBodyToRequest() {
+        if (request instanceof HttpEntityEnclosingRequestBase)
+            ((HttpEntityEnclosingRequestBase) request).setEntity(new ByteArrayEntity(body));
     }
 
     private HttpClient httpClient() {
--- src/test/java/pl/gebickionline/restclient/RestClientTest.java
+++ src/test/java/pl/gebickionline/restclient/RestClientTest.java
@@ -2,53 +2,69 @@ package pl.gebickionline.restclient;
 
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import org.junit.*;
+import wiremock.org.json.JSONObject;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static org.junit.Assert.assertEquals;
 
 public class RestClientTest {
     public static final String TARGET_HOST = "http://localhost:8000";
     @Rule
     public WireMockRule service = new WireMockRule(8000);
+    private String jsonObject;
+    private String xmlObject;
 
-    @Test
-    public void testCreatingInstance() throws Exception {
-        RestClient restClient = RestClient.get(TARGET_HOST);
-        restClient = RestClient.post(TARGET_HOST);
-        restClient = RestClient.put(TARGET_HOST);
-        restClient = RestClient.delete(TARGET_HOST);
+    @Before
+    public void setUp() throws Exception {
+        jsonObject = new JSONObject().put("field", "value").toString();
+        xmlObject = "<field>value</field>";
     }
 
     @Test
-    public void testDefaultAcceptTypeIsPlainText() throws Exception {
-        stubForGetMethod("application/json", "{\"response\":\"ok\"}");
-        stubForGetMethod("text/plain", "ok");
-        stubForGetMethod("application/xml", "<response>ok</response>");
-
-        final Response response = RestClient.get(TARGET_HOST + "/resource").send();
-
-        assertEquals(200, response.statusCode());
-        assertEquals("ok", response.asString());
+    public void givenNoAcceptHeader_whenGetRequestInvoked_setsAcceptHeaderToPlainText() throws Exception {
+        stubForGetMethod(MediaType.APPLICATION_JSON);
+        stubForGetMethod(MediaType.TEXT_PLAIN);
+        stubForGetMethod(MediaType.APPLICATION_XML);
+        RestClient.get(TARGET_HOST + "/resource").send();
+        verify(getRequestedFor(urlEqualTo("/resource")).withHeader("accept", equalTo(MediaType.TEXT_PLAIN)));
     }
 
     @Test
-    public void testGetMethod() throws Exception {
-        stubForGetMethod("application/json", "{\"response\":\"ok\"}");
+    public void givenAcceptHeader_whenGetRequestInvoked_sendsRequestWithAcceptHeader() throws Exception {
+        stubForGetMethod(MediaType.APPLICATION_JSON);
+        stubForGetMethod(MediaType.TEXT_PLAIN);
+        stubForGetMethod(MediaType.APPLICATION_XML);
 
-        final Response response = RestClient.get(TARGET_HOST + "/resource")
-                .header("accept", "application/json")
+        RestClient.get(TARGET_HOST + "/resource")
+                .header("accept", MediaType.APPLICATION_JSON)
                 .send();
 
-        assertEquals(200, response.statusCode());
-        assertEquals("{\"response\":\"ok\"}", response.asString());
+        verify(getRequestedFor(urlEqualTo("/resource")).withHeader("accept", equalTo(MediaType.APPLICATION_JSON)));
     }
 
-    private void stubForGetMethod(String contentType, String responseBody) {
+
+    @Test
+    public void givenBody_whenPutRequestInvoked_sendsRequestWithBody() throws Exception {
+        stubForPutMethod(MediaType.APPLICATION_JSON, jsonObject);
+        RestClient.put(TARGET_HOST + "/resource")
+                .header("content-type", MediaType.APPLICATION_JSON)
+                .body(jsonObject)
+                .send();
+        verify(putRequestedFor(urlEqualTo("/resource"))
+                .withHeader("content-type", equalTo(MediaType.APPLICATION_JSON))
+                .withRequestBody(equalTo(jsonObject)));
+    }
+
+    private void stubForPutMethod(String contentType, String jsonObject) {
+        service.stubFor(put(urlEqualTo("/resource"))
+                .withHeader("content-type", equalTo(contentType))
+                .withRequestBody(equalTo(jsonObject))
+                .willReturn(aResponse().withStatus(200)));
+    }
+
+    private void stubForGetMethod(String contentType) {
         service.stubFor(get(urlEqualTo("/resource"))
                 .withHeader("accept", equalTo(contentType))
-                .willReturn(aResponse()
-                        .withHeader("content-type", contentType)
-                        .withStatus(200)
-                        .withBody(responseBody)));
+                .willReturn(aResponse().withStatus(200)));
     }
+
 }
